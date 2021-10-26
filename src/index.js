113e149041949063bcd2750d3e8deddeeb9118ef
Milad <miladnasri4030@gmail.com>
2021-10-26 21:35:11 +0330

    add first project in github

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var interval;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: prompt("enter a number"),
+    };
+  }
+
+  componentDidMount() {
+    interval = setInterval(() => {
+      this.setState({
+        counter: this.state.counter - 1,
+      });
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    if (this.state.counter === 0) {
+      clearInterval(interval);
+    }
+  }
+  render() {
+    return <h2 className="counter">{this.state.counter}</h2>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>Hello My Friends</h1>
+        <div className="counter">
+          <Counter />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
